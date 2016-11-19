a2e8384f0b97827ae64d7f0989fe698d4d749208
Sebastian Kipping <kipping.sebastian@gmail.com>
2016-11-19 20:59:34 +0100

    Init Commit

---
+++ src/main/java/de/sisekipp/YoutubedowloaderApplication.java
@@ -0,0 +1,12 @@
+package de.sisekipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubedowloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubedowloaderApplication.class, args);
+	}
+}
