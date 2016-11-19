47bd9e6dcf9885504072e5a77751c921997d8be0
Sebastian Kipping <kipping.sebastian@gmail.com>
2016-11-19 21:50:40 +0100

    Add base controller

--- src/main/java/de/sisekipp/controller/HomeController.java
+++ src/main/java/de/sisekipp/controller/HomeController.java
@@ -1,7 +1,17 @@
 package de.sisekipp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * Created by sebastian on 19.11.16.
  */
-public class MainController {
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "main";
+    }
 }
--- src/main/java/de/sisekipp/controller/SearchController.java
+++ src/main/java/de/sisekipp/controller/SearchController.java
@@ -1,7 +1,18 @@
 package de.sisekipp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
  * Created by sebastian on 19.11.16.
  */
+@Controller
 public class SearchController {
+
+    @GetMapping(path = "/search")
+    public String search(@RequestParam("url") String url, Model model) {
+        return "redirect:/";
+    }
 }
